fcd3ba38348c3b554b12fdb092575ad35862a752
league <league@imac-18.attlocal.net>
2023-05-24 16:59:49 -0700

    continue to list of goals(project)

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -11,9 +11,17 @@ public class FlappyBird extends PApplet {
     int birdYVelocity = -10;
     int gravity = 1;
     
-    int pipeX = 800;
-    int pipeGap = 100;
+    int PipeX = 800;
+    
+
+    int pipeGap = 150;
     int upperPipeHeight = (int) random(100, 400);
+    int lowerY = upperPipeHeight + pipeGap;
+    
+    int floorX = 0;
+    int floorY = 550;
+    
+    int score = 0;
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -26,17 +34,37 @@ public class FlappyBird extends PApplet {
 
     @Override
     public void draw() {
+    	
+    	if(intersectsPipes() == false && touchFloor() == false) {
     	background(13, 166, 255);
     	fill(44,176,26);
-    	rect(pipeX, 0, 50, upperPipeHeight);
-    	pipeX -= 5;
+    	//top pipe
+    	rect(PipeX, 0, 50, upperPipeHeight);
+    	PipeX -= 5;
     	teleportPipe();
     	
+    	// bottom pipe
+    	rect(PipeX, lowerY, 50, 600);
+    	
+    	//floor shape
+    	fill(43, 143, 43);
+    	rect(0, 550, 800,50);
+ 
     	fill(255, 231, 13);
     	stroke(200,213,231);
     	ellipse(birdX, birdY, 50,50);  
     	birdYVelocity += gravity;
     	birdY += birdYVelocity; 
+    	
+    	// text for the score
+    	textSize(20);
+    	text("Your score:" + score, 20,597);
+    	}	
+    	else if(intersectsPipes() == true || touchFloor() == true) {
+    		background(13, 166, 255);
+    		textSize(40);
+    		text("Game over \n Your score: " + score, WIDTH / 2 - 100, HEIGHT / 2);
+    	}
     }
 
     public void mousePressed(){
@@ -44,14 +72,34 @@ public class FlappyBird extends PApplet {
     }
     
     public void teleportPipe() {
-    	if (pipeX < 0) {
-    		pipeX  = 850;
+    	if (PipeX < 0) {
+    		PipeX  = 850;
     	    upperPipeHeight = (int) random(100, 400);
+    	    lowerY = upperPipeHeight + pipeGap;
+			score +=1;
 
     	}
 
     }
+    
+    boolean intersectsPipes() { 
+        if (birdY < upperPipeHeight && birdX > PipeX && birdX < (PipeX+50)){
+           return true; }
+       else if (birdY>lowerY && birdX > PipeX && birdX < (PipeX+50)) {
+           return true; }
+       else { return false;}
+}
+    
+    boolean touchFloor() {
+    	if (birdX >floorX && birdX < floorX + 800 && birdY > floorY && birdY < floorY + 100 ) {
+    		return true;}
+    	
+    	else { return false; }
+    		
+    	
+}
+
     static public void main(String[] args) {
         PApplet.main(FlappyBird.class.getName());
     }
-}
+    }
--- src/_02_return_types/_1_finish_line/FinishLine.java
+++ src/_02_return_types/_1_finish_line/FinishLine.java
@@ -17,20 +17,25 @@ public class FinishLine {
 	
 	public static void main(String[] args) {
 		//1. Call the drawFinishLine() method
-		
+		drawFinishLine();
 		//2. Call the crazyMove() method to move the robot
-		
+		crazyMove();
 		//3. Call the getTotalDistance() and save what is returned into a variable
-		
+		getTotalDistance();
+		int t = totalDistance;
 		//4. Call the hasCrossedFinishLine() method and save what is returned into a variable
-		
+		hasCrossedFinishLine();
+		boolean h = hasCrossedFinishLine();
 		//5. If the robot has crossed the finish line... 
-			
+			if (h == true) {
 			//6. Use a pop up to say the robot finished and how far it went
-		
+			JOptionPane.showMessageDialog(null, "The robot finished. It went " + t);
+			}
 		
 		//7. Else use a pop up to say the robot did not finish and how far it went
-		
+			else {
+				JOptionPane.showMessageDialog(null,"The robot did not finish. It went " + t );
+			}
 	}
 	
 	static void crazyMove() {
